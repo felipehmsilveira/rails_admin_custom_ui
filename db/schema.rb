96ce950ef27867627d97cd17e83a59278a561643
Felipe <felipehmsilveira@gmail.com>
2020-07-06 02:52:06 +0100

    Configuração de visualização do admin, campos adicionados e lógica da aplicação do desconto de porcentagem e valor fixo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_05_235242) do
+ActiveRecord::Schema.define(version: 2020_07_06_014508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2020_07_05_235242) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "sale_id"
     t.index ["product_id"], name: "index_product_quantities_on_product_id"
     t.index ["user_id"], name: "index_product_quantities_on_user_id"
   end
@@ -81,6 +82,7 @@ ActiveRecord::Schema.define(version: 2020_07_05_235242) do
     t.integer "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "price"
   end
 
   create_table "sales", force: :cascade do |t|
